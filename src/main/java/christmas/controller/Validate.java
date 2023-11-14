cb574a12441197538094751e5c44fe263479e150
Min <mijien0179@gmail.com>
2023-11-15 04:47:15 +0900

    fix : 요구 사항에 맞게 throw text 재설정

--- src/main/java/christmas/controller/Validate.java
+++ src/main/java/christmas/controller/Validate.java
@@ -10,11 +10,9 @@ public class Validate {
     }
 
     public static void rangeOfDecember(int number) {
-        if (number < 1) {
-            throw new IllegalArgumentException("날짜는 1일 미만일 수 없습니다");
-        }
-        if (31 < number) {
-            throw new IllegalArgumentException("날짜는 31일 초과일 수 없습니다.");
+        if (number < 1 || 31 < number) {
+            // NOTE: REQUIRED TEXT
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
@@ -22,7 +20,7 @@ public class Validate {
         try {
             Menu.valueOf(name);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(name + " 존재하지 않는 메뉴명입니다");
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
@@ -33,20 +31,20 @@ public class Validate {
             var diffMenuName = item.getMenu()
                     .name();
             if (diffMenuName.equals(menuName)) {
-                throw new IllegalArgumentException("메뉴는 중복으로 주문할 수 없습니다.");
+                throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
             }
         }
     }
 
     public static void menuCount(int count){
         if(count < 1){
-            throw new IllegalArgumentException("메뉴별 개수는 1개 이상이어야 합니다.");
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
     public static void menuAndCountFormat(String str){
         if(!str.contains("-")){
-            throw new IllegalArgumentException("주문시 메뉴-주문수 형식을 지켜주세요.");
+            throw new IllegalArgumentException("유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
